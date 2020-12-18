919fce937201c47ebaa2791d3c3d5de1addb57a8
Ryan <sebastiansryan@gmail.com>
2020-12-17 20:36:35 -0800

    Added Test Routes

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const app = express();
 connectDB();
 
 // Define routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/users', require('./routes/api/users'));
 
 const PORT = process.env.PORT || 5000;
