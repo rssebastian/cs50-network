62aab03ef92079ed1534f6395d70d63d3c2e1704
Ryan <sebastiansryan@gmail.com>
2020-12-28 17:34:24 -0800

    added register form onChange and onSubmit handlers

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCode } from '@fortawesome/free-solid-svg-icons';
 
@@ -6,19 +7,19 @@ export const Navbar = () => {
   return (
     <nav className='navbar bg-dark'>
       <h1>
-        <a href='index.html'>
+        <Link to='/'>
           <FontAwesomeIcon icon={faCode} /> CS50 Network
-        </a>
+        </Link>
       </h1>
       <ul>
         <li>
-          <a href='profiles.html'>Developers</a>
+          <a href='!#'>Developers</a>
         </li>
         <li>
-          <a href='register.html'>Register</a>
+          <Link to='/register'>Register</Link>
         </li>
         <li>
-          <a href='login.html'>Login</a>
+          <Link to='/login'>Login</Link>
         </li>
       </ul>
     </nav>
