6c444c72337b4bf5f3d780062dc82efba13be38a
Ryan <sebastiansryan@gmail.com>
2020-12-30 23:32:53 -0800

    generated a PostItem component to render out each post

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,14 +2,33 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
 import { getPosts } from '../../actions/post';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  return <div></div>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className='large text-primary'>Posts</h1>
+      <p className='lead'>
+        <FontAwesomeIcon icon={faUser} /> Welcome to the community
+      </p>
+      {/*PostForm*/}
+      <div className='posts'>
+        {posts.map((post) => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
